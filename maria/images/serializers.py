8214a57bcbf90f0f4b5d2270c470a6095b795cbb
David Bautista <davidbaar07@gmail.com>
2025-02-19 17:41:31 -0600

    ImagesSerialer

---
+++ maria/images/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from .models import Images
+
+
+class ImagesSerializers(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Images
+        fields = [
+            'image_path'
+        ]
+
--- maria/images/views.py
+++ maria/images/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets, status
+from rest_framework.decorators import action
+from .permissions import IsAdmin
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from .models import Images
+from .serializers import ImagesSerializers
+
+
+class ImagesViewSet(viewsets.ModelViewSet):
+    queryset = Images.object.all()
+    serializer_class = ImagesSerializers
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAdmin]
+
